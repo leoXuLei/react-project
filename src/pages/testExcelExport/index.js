4bcebf5d2d4bde61519ce4f028b0a7900d2236b1
leoXuLei <1611928589@qq.com>
2021-04-06 16:17:19 +0800

    xlsx导出Excel实现单元格带样式功能

--- src/pages/testExcelExport/index.js
+++ src/pages/testExcelExport/index.js
@@ -6,17 +6,16 @@ import { DATA_LIST, MERGES_LIST } from "./contants";
 import BigNumber from "bignumber.js";
 
 const TestExcelExport = () => {
-  // 如果发请求获取数据，需要把请求到的数据转成和 value_list 一样的数据格式，在作为参数，传入 downloadExl 方法中
-  // value_list 中一个数组表示excel中一行数据
-  const downloadExl = (value_list, filename, type) => {
-    //编码单元格
-
-    var tmp_data = {};
-    value_list.forEach(function (value, r) {
+  // 如果发请求获取数据，需要把请求到的数据转成和 dataList 一样的数据格式，在作为参数，传入 downloadExl 方法中
+  // dataList 中一个数组表示excel中一行数据
+  const downloadExl = (dataList, filename, type) => {
+    // 编码单元格
+    var codingCellData = {};
+    dataList.forEach(function (value, r) {
       value.forEach(function (v, c) {
         var cell_obj = { c: c, r: r };
         var cell_text = XLSX.utils.encode_cell(cell_obj);
-        tmp_data[cell_text] = {
+        codingCellData[cell_text] = {
           v: v,
         };
       });
@@ -61,42 +60,43 @@ const TestExcelExport = () => {
         vertical: "center",
       },
     };
-    console.log(`tmp_data`, tmp_data);
+    console.log(`codingCellData`, codingCellData);
     //设置H1单元格样式
-    tmp_data["E1"].s = {
+    codingCellData["E1"].s = {
       ...hvCenterStyle,
     };
 
-    //设置【1-8】行【A-E】列的样式
+    // 设置【A-D 共4】列的【1-dataLen】行的样式
     var col_list = [];
-    // var length = value_list[0].length;
     for (var i = 0; i < 4; i++) {
       var col_text = XLSX.utils.encode_col(i);
       col_list.push(col_text);
     }
     console.log(`col_list`, col_list);
-    col_list.forEach(function (value, index) {``
+    col_list.forEach(function (value, index) {
+      ``;
       for (let i = 2; i < 52; i++) {
         var attr3 = value + i;
-        tmp_data[attr3].s = {
+        codingCellData[attr3].s = {
           ...hvCenterStyle,
         };
       }
     });
+    // 设置【E：4】列 多个多行合并的样式
     const mergeStartKeys = [1, 3, 5, 11, 19, 25, 33, 36, 41].map((v) => v + 1);
     mergeStartKeys.forEach((v) => {
-      tmp_data[`E${v}`].s = {
+      codingCellData[`E${v}`].s = {
         ...hvCenterStyle,
       };
     });
     //获取所有单元格编码,比如["A1", "B1", "C1", "D1", "E1", "F1"]
-    var output_pos = Object.keys(tmp_data);
+    var output_pos = Object.keys(codingCellData);
     var workbook = {
       SheetNames: ["sheet1"], //保存工作表的名称
       Sheets: {
         sheet1: Object.assign(
           {},
-          tmp_data, //单元格内容
+          codingCellData, //单元格内容
           {
             "!ref": output_pos[0] + ":" + output_pos[output_pos.length - 1], //工作表范围
           }
